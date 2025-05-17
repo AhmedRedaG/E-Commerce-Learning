f44dd300928fe3211ebce606896158e1aa91f2bc
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-05-17 12:36:35 +0300

    feat: implement signup functionality and update login views

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,26 +7,44 @@ export const getLogin = (req, res) => {
   res.render("auth/login", {
     pageTitle: "Login",
     currentPath: "/login",
+    signup: false,
   });
 };
 
 export const postLogin = (req, res) => {
-  // const email = req.body.email;
-  // const password = req.body.password;
-  // User.findOne({ email: email })
-  //   .then((user) => {
-  //     if (!user) {
-  //       return res.redirect("/login");
-  //     }
-  //     if (user.password !== password) {
-  //       return res.redirect("/login");
-  //     }
-  //     res.redirect("/");
-  //   })
-  //   .catch((err) => {
-  //     res.render("error", { pageTitle: "Error", currentPath: "", err });
-  //   });
+  const { email, password } = req.body;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        return res.redirect("/signup");
+      }
+      if (user.password !== password) {
+        return res.redirect("/login");
+      }
+      res.redirect("/");
+    })
+    .catch((err) => {
+      res.render("error", { pageTitle: "Error", currentPath: "", err });
+    });
+};
 
-  req.session.isLoggedIn = true;
-  res.redirect("/");
+export const getSignup = (req, res) => {
+  res.render("auth/login", {
+    pageTitle: "Signup",
+    currentPath: "/signup",
+    signup: true,
+  });
+};
+
+export const postSignup = (req, res) => {
+  const { name, email, password } = req.body;
+  const user = new User({ name, email, password, cart: [] });
+  user
+    .save()
+    .then(() => {
+      res.redirect("/login");
+    })
+    .catch((err) => {
+      res.render("error", { pageTitle: "Error", currentPath: "", err });
+    });
 };
